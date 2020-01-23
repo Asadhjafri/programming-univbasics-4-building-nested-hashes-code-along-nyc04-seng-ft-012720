9b38cc363ee6aeb0fce9ecf5f0530e6eb2b431a7
Asad Jafri <asadhjafri@gmail.com>
2020-01-23 23:57:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :matriarch => { name: "Lady Montague", age:"54"
         #your key/value pairs here
         },
-      :hero => { name: "Romeo", age:"15" status:alive
+      :hero => { name: "Romeo", age:"15" status:"alive"
         #your key/value pairs here
         },
       :hero_friends => [
@@ -16,16 +16,19 @@ def third_challenge
       ]
    },
    :capulet => {
-      :patriarch => {
+      :patriarch => {name: "Lord Capulet", age:"50"
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {name: "Lady Capulet", age:"51"
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {name: "Juliet", age:"15", status:"alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name:"Steven", age:"30", attitude:"confused"}
+        {name:"Nurse", age:"44", attitude:"worried"}
+      ]
    }
   }
 
