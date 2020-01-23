e4fa78edb17135244991c0b653df8293ddf2d168
Asad Jafri <asadhjafri@gmail.com>
2020-01-23 23:53:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => [
-        {name:"Benvolio", age:"17", attitude:worried}
+        {name:"Benvolio", age:"17", attitude:"worried"}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
    :capulet => {
